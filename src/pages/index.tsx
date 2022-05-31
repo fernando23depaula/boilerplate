6ee79aefa9a080251f962e7a441e74c3fad1dc0c
Fernando de Paula <fernando23.depaula@gmail.com>
2022-05-31 10:36:12 -0300

    Add lint staged and conf

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import styles from '../../styles/Home.module.css'
 
 export default function Home({ title = 'React Avançado!' }) {
   const bla = 'foo'
+  const bla2 = 'foo'
   return (
     <div className={styles.container}>
       <Head>
